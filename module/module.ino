109964afb19c09c7dcbf4d43b8943a854c591cb1
Pierre-Rudolf Gerlach <pgerlach@gmail.com>
2016-10-08 14:47:36 +0200

    read module id from EEPROM

--- module/module.ino
+++ module/module.ino
@@ -1,7 +1,10 @@
 #include <SPI.h>
-#include <Mirf.h>
-#include <nRF24L01.h>
-#include <MirfHardwareSpiDriver.h>
+#include <RF24.h>
+#include <EEPROM.h>
+
+// timings
+#define BRAKE_TIMING (500)
+#define RELEASE_TIMING (500)
 
 // common
 #define PIN_NRF24L01_CS (9)
@@ -14,6 +17,10 @@
 #define ID_MODULE_1 "mod1"
 #define ID_MODULE_2 "mod2"
 
+#define DEBUG_MOTORS(msg) // Serial.println(msg)
+#define DEBUG_MSGS(msg) Serial.println(msg)
+
+
 // module
 const int PIN_ENA = 2;
 const int PIN_IN1 = 3;
@@ -25,6 +32,8 @@ const int PIN_IN4 = 7;
 const int MOTOR_1_PINS[] = { PIN_ENA, PIN_IN1, PIN_IN2 };
 const int MOTOR_2_PINS[] = { PIN_ENB, PIN_IN4, PIN_IN3 };
 
+const int PIN_LED_NO_MSG = A0;
+
 enum e_state {
   E_STATE_FREE,
   E_STATE_BRAKING,
@@ -34,54 +43,79 @@ enum e_state {
 
 long actionStartTime = millis();
 
+long lastMessageDisplayTimer = 0;
+
+RF24 radio(PIN_NRF24L01_CE,PIN_NRF24L01_CS);
+
+struct s_module_eeprom_data {
+  byte magic = 0x42; // MUST be 0x42
+  char id[5];  // 0 terminated string
+} eepromData;
+
+
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  // init nRF24L01
-  Mirf.cePin = PIN_NRF24L01_CE;
-  Mirf.csnPin = PIN_NRF24L01_CS;
-  Mirf.spi = &MirfHardwareSpi;
-  Mirf.init();
+  memset(&eepromData, 0, sizeof(eepromData));
+  EEPROM.get(0, eepromData);
+  if (eepromData.magic != 0x42) {
+    Serial.println("bad magic");
+    Serial.println(eepromData.magic);
+    Serial.println("FAIL");
+    while (true) ;
+  }
+  Serial.print("module id: "); Serial.println(eepromData.id);
 
-  // this is module 1
-  Mirf.setRADDR((byte *)ID_MODULE_2);
-
-  // messages are only 1 byte
-  Mirf.payload = sizeof(byte);
-
-  /*
-   * To change channel:
-   * Mirf.channel = 10;
-   */
-   
-  Mirf.config();
-
+  radio.begin();
+  radio.setPALevel(RF24_PA_LOW);
+  radio.openReadingPipe(1,(byte*)eepromData.id);
+ 
   // setup PINS
   for (int i=0; i<3; i++) {
     pinMode(MOTOR_1_PINS[i], OUTPUT);
     pinMode(MOTOR_2_PINS[i], OUTPUT);
   }
   motors_init();
+  pinMode(PIN_LED_NO_MSG, OUTPUT);
+  digitalWrite(PIN_LED_NO_MSG, LOW);
 
   state = E_STATE_FREE;
 
-  Serial.println("WELCOME TO THE MODULE 2 LOGS");
+  lastMessageDisplayTimer = millis();
+
+  radio.startListening();
+  Serial.println("WELCOME TO THE MODULE 1 LOGS");
 }
 
-void loop() {
-  if (Mirf.dataReady()) {
-    byte msg;
-    Mirf.getData((byte *) &msg);
+int loopsWithNoActions = 0;
 
+void loop() {
+  if (radio.available()) {
+    digitalWrite(PIN_LED_NO_MSG, LOW);
+    lastMessageDisplayTimer = millis();
+    byte msg;
+    radio.read(&msg, sizeof(byte));
+
+    actionStartTime = millis();
     switch (msg) {
       case MSG_BRAKE:
-        Serial.println("brake");
+        DEBUG_MSGS("msg brake");
         msg_brake();
         break;
       case MSG_FREE:
-        Serial.println("release");
+        DEBUG_MSGS("msg release");
         msg_release();
         break;
+      default:
+        DEBUG_MSGS("msg UNKNOWN");
+        DEBUG_MSGS((char)msg);
+        break;
+    }
+  } else {
+    if (millis() - lastMessageDisplayTimer > 1500) {
+      digitalWrite(PIN_LED_NO_MSG, HIGH);
+      DEBUG_MSGS("1.5 second w/ no data");
+      lastMessageDisplayTimer = millis();
     }
   }
 
@@ -105,7 +139,7 @@ void msg_brake() {
   switch (state) {
     case E_STATE_BRAKING:
     case E_STATE_STOPPED:
-      // nothing to do
+      DEBUG_MOTORS("BREAK asked but already stopped");
       break;
     case E_STATE_FREE:
     case E_STATE_RELEASING:
@@ -120,7 +154,7 @@ void msg_release() {
   switch (state) {
     case E_STATE_FREE:
     case E_STATE_RELEASING:
-      // nothing to do
+      DEBUG_MOTORS("RELEASE asked but already released");
       break;
     case E_STATE_BRAKING:
     case E_STATE_STOPPED:
@@ -132,28 +166,26 @@ void msg_release() {
 
 
 void motors_init_brake() {
-  Serial.println("init brake");
-  actionStartTime = millis();
+  DEBUG_MOTORS("init brake");
   motor(MOTOR_1_PINS, MOTOR_GO_FWD);
   motor(MOTOR_2_PINS, MOTOR_GO_FWD);
 }
 
 void motors_init_release() {
-  Serial.println("init release");
-  actionStartTime = millis();
+  DEBUG_MOTORS("init release");
   motor(MOTOR_1_PINS, MOTOR_GO_BCK);
   motor(MOTOR_2_PINS, MOTOR_GO_BCK);
 }
 
 void motors_stop() {
-  Serial.println("motors stop");
+  DEBUG_MOTORS("motors stop");
   motor(MOTOR_1_PINS, MOTOR_STOP);
   motor(MOTOR_2_PINS, MOTOR_STOP);
 }
 
 void motors_init() {
   motors_init_release();
-  delay(5000);
+  delay(2000);
   motors_stop();
 }
 
@@ -161,9 +193,18 @@ void motors_loop() {
   if (state == E_STATE_STOPPED || state == E_STATE_FREE) {
     return ;
   }
+  long timing = 0;
+  switch (state) {
+    case E_STATE_BRAKING:
+      timing = BRAKE_TIMING;
+      break;
+    case E_STATE_RELEASING:
+      timing = RELEASE_TIMING;
+      break;
+  }
   // is it time to stop ?
-  if (millis() - actionStartTime > 3000) {
-    Serial.println("action done -> stop the motors");
+  if (millis() - actionStartTime > timing) {
+    DEBUG_MOTORS("action done -> stop the motors");
     motors_stop();
     switch (state) {
       case E_STATE_BRAKING:
